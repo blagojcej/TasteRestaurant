e0f1e8d2b07d2278c300c95ab11c504d010bb2d2
Blagojce Jovanoski <blagojcejovanoski@gmail.com>
2018-08-10 13:30:02 +0200

    Added IndexPage for TasteRestaurant.

--- TasteRestaurant/Pages/Index.cshtml.cs
+++ TasteRestaurant/Pages/Index.cshtml.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
+using TasteRestaurant.ViewModel;
 
 namespace TasteRestaurant.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _context;
 
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public IndexViewModel IndexVM { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IndexVM = new IndexViewModel()
+            {
+                MenuItems = await _context.MenuItems.Include(c => c.CategoryType).Include(f => f.FoodType).ToListAsync(),
+                CategoryTypes = await _context.CategoryTypes.OrderBy(c => c.DisplayOrder).ToListAsync()
+            };
         }
     }
 }
---
+++ TasteRestaurant/ViewModel/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.ViewModel
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<MenuItem> MenuItems { get; set; }
+        public IEnumerable<CategoryType> CategoryTypes { get; set; }
+    }
+}
